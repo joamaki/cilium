f57e4c01854a3fa747d83e324668566da4ea55d8
Jussi Maki <jussi@isovalent.com>
2024-08-09 16:30:56 +0200

    ciliumenvoyconfig: Tests for the experimental controller
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- pkg/ciliumenvoyconfig/experimental.go
+++ pkg/ciliumenvoyconfig/experimental.go
@@ -18,16 +18,61 @@ import (
 	"github.com/cilium/statedb"
 	"github.com/cilium/statedb/index"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/cache"
 )
 
-var experimentalCell = cell.Module(
-	"experimental",
-	"Integration to experimental LB control-plane",
+var (
+	experimentalModule = cell.Module(
+		"experimental",
+		"Integration to experimental LB control-plane",
 
-	cell.ProvidePrivate(newCECTable),
-	cell.Provide(newCECController),
-	cell.Invoke(registerCECReflector),
+		cell.ProvidePrivate(cecListerWatchers),
+		experimentalTableCells,
+		experimentalControllerCells,
+	)
+
+	experimentalControllerCells = cell.Group(
+		cell.Provide(newCECController),
+
+		// Wire up cecController with the writer. This needs to be done
+		// separately from the constructor as it's providing a ServiceHook.
+		// TODO: Reconsider use of group values and instead just have Writer.AddServiceHook etc.?
+		cell.Invoke((*cecController).setWriter),
+	)
+
+	experimentalTableCells = cell.Group(
+		cell.ProvidePrivate(
+			newCECTable,
+			statedb.RWTable[*cec].ToTable,
+		),
+		cell.Invoke(
+			registerCECReflector,
+		),
+	)
 )
+
+// Types for the ListerWatchers of the CEC resources. Abstracted so that tests can
+// inject custom ones.
+type (
+	cecListerWatcher  cache.ListerWatcher
+	ccecListerWatcher cache.ListerWatcher
+
+	listerWatchers struct {
+		cec  cecListerWatcher
+		ccec ccecListerWatcher
+	}
+)
+
+func cecListerWatchers(cs client.Clientset) (out struct {
+	cell.Out
+	LW listerWatchers
+}) {
+	if cs.IsEnabled() {
+		out.LW.cec = utils.ListerWatcherFromTyped(cs.CiliumV2().CiliumEnvoyConfigs(""))
+		out.LW.ccec = utils.ListerWatcherFromTyped(cs.CiliumV2().CiliumClusterwideEnvoyConfigs())
+	}
+	return
+}
 
 type cec struct {
 	name, namespace string
@@ -82,8 +127,8 @@ func newCECTable(db *statedb.DB) (statedb.RWTable[*cec], error) {
 	return tbl, db.RegisterTable(tbl)
 }
 
-func registerCECReflector(ecfg experimental.Config, p *cecResourceParser, log *slog.Logger, cs client.Clientset, g job.Group, db *statedb.DB, tbl statedb.RWTable[*cec]) error {
-	if !cs.IsEnabled() || !ecfg.EnableExperimentalLB {
+func registerCECReflector(ecfg experimental.Config, p *cecResourceParser, log *slog.Logger, lws listerWatchers, g job.Group, db *statedb.DB, tbl statedb.RWTable[*cec]) error {
+	if lws.cec == nil || !ecfg.EnableExperimentalLB {
 		return nil
 	}
 	transform := func(obj any) (*cec, bool) {
@@ -125,11 +170,12 @@ func registerCECReflector(ecfg experimental.Config, p *cecResourceParser, log *s
 
 	// CiliumEnvoyConfig reflection
 	err := k8s.RegisterReflector(
+		"cec",
 		g,
 		db,
 		tbl,
 		k8s.ReflectorConfig[*cec]{
-			ListerWatcher: utils.ListerWatcherFromTyped(cs.CiliumV2().CiliumEnvoyConfigs("")),
+			ListerWatcher: lws.cec,
 			Transform:     transform,
 			QueryAll: func(txn statedb.ReadTxn, tbl statedb.Table[*cec]) statedb.Iterator[*cec] {
 				return statedb.Filter(
@@ -145,11 +191,12 @@ func registerCECReflector(ecfg experimental.Config, p *cecResourceParser, log *s
 
 	// CiliumClusterwideEnvoyConfig reflection
 	return k8s.RegisterReflector(
+		"ccec",
 		g,
 		db,
 		tbl,
 		k8s.ReflectorConfig[*cec]{
-			ListerWatcher: utils.ListerWatcherFromTyped(cs.CiliumV2().CiliumClusterwideEnvoyConfigs()),
+			ListerWatcher: lws.ccec,
 			Transform:     transform,
 			QueryAll: func(txn statedb.ReadTxn, tbl statedb.Table[*cec]) statedb.Iterator[*cec] {
 				return statedb.Filter(
@@ -167,33 +214,39 @@ type cecControllerParams struct {
 	DB       *statedb.DB
 	JobGroup job.Group
 	CECs     statedb.Table[*cec]
-	Writer   *experimental.Writer
 	Log      *slog.Logger
 }
 
 type cecController struct {
-	cecControllerParams
+	params cecControllerParams
+	writer *experimental.Writer
 }
 
 type cecControllerOut struct {
 	cell.Out
 
+	C    *cecController
 	Hook experimental.ServiceHook `group:"service-hooks"`
 }
 
 func newCECController(params cecControllerParams) cecControllerOut {
-	c := &cecController{params}
+	c := &cecController{params, nil}
 	params.JobGroup.Add(job.OneShot("control-loop", c.loop))
 	return cecControllerOut{
+		C:    c,
 		Hook: c.onServiceUpsert,
 	}
+}
+
+func (c *cecController) setWriter(w *experimental.Writer) {
+	c.writer = w
 }
 
 func (c *cecController) loop(ctx context.Context, health cell.Health) error {
 	var (
 		// Services table is used for looking up the service on which we're
 		// setting the proxy redirection.
-		svcs statedb.Table[*experimental.Service] = c.Writer.Services()
+		svcs statedb.Table[*experimental.Service] = c.writer.Services()
 
 		// Iterator for changes (upsert/delete) to the cilium envoy config table.
 		// We process each change and look up a referenced service to set/unset the
@@ -202,9 +255,9 @@ func (c *cecController) loop(ctx context.Context, health cell.Health) error {
 	)
 
 	{
-		wtxn := c.DB.WriteTxn(c.CECs, c.Writer.Services())
+		wtxn := c.params.DB.WriteTxn(c.params.CECs, c.writer.Services())
 		var err error
-		changes, err = c.CECs.Changes(wtxn)
+		changes, err = c.params.CECs.Changes(wtxn)
 		wtxn.Commit()
 		if err != nil {
 			return err
@@ -212,7 +265,7 @@ func (c *cecController) loop(ctx context.Context, health cell.Health) error {
 	}
 
 	for {
-		txn := c.Writer.WriteTxn()
+		txn := c.writer.WriteTxn()
 		for change, _, ok := changes.Next(); ok; change, _, ok = changes.Next() {
 			cec := change.Object
 
@@ -225,16 +278,16 @@ func (c *cecController) loop(ctx context.Context, health cell.Health) error {
 				if found {
 					svc = svc.Clone()
 					if change.Deleted {
-						c.Log.Debug("Removing proxy redirection",
+						c.params.Log.Debug("Removing proxy redirection",
 							"namespace", svcl.Namespace, "name", svcl.Name, "listener", svcl.Listener)
 						svc.L7ProxyPort = 0
 					} else {
 						proxyPort := lookupProxyPort(cec, svcl)
-						c.Log.Debug("Setting proxy redirection",
+						c.params.Log.Debug("Setting proxy redirection",
 							"namespace", svcl.Namespace, "name", svcl.Name, "proxyPort", proxyPort, "listener", svcl.Listener)
 						svc.L7ProxyPort = proxyPort
 					}
-					c.Writer.UpsertService(txn, svc)
+					c.writer.UpsertService(txn, svc)
 				}
 			}
 		}
@@ -243,7 +296,7 @@ func (c *cecController) loop(ctx context.Context, health cell.Health) error {
 		select {
 		case <-ctx.Done():
 			return nil
-		case <-changes.Watch(c.DB.ReadTxn()):
+		case <-changes.Watch(c.params.DB.ReadTxn()):
 		}
 	}
 }
@@ -252,7 +305,7 @@ func lookupProxyPort(cec *cec, svcl *ciliumv2.ServiceListener) uint16 {
 	if svcl.Listener != "" {
 		// Listener names are qualified after parsing, so qualify the listener reference as well for it to match
 		svcListener, _ := api.ResourceQualifiedName(
-			svcl.Namespace, svcl.Name, svcl.Listener, api.ForceNamespace)
+			cec.namespace, cec.name, svcl.Listener, api.ForceNamespace)
 
 		for _, l := range cec.resources.Listeners {
 			if l.Name == svcListener {
@@ -269,15 +322,21 @@ func lookupProxyPort(cec *cec, svcl *ciliumv2.ServiceListener) uint16 {
 
 // onServiceUpsert is called when the service is upserted, but before it is commited.
 // We set the proxy port on the service if there's a matching CEC.
+//
+// TODO: When loop() sets the ProxyPort we get called back into this. Could consider
+// optimizing that out.
 func (c *cecController) onServiceUpsert(txn statedb.ReadTxn, svc *experimental.Service) {
+	c.params.Log.Info("onServiceUpsert", "name", svc.Name)
+
 	if svc.L7ProxyPort != 0 {
 		// Proxy port already set. Changes to proxy port are handled in reconcile()
 		return
 	}
 
 	// Look up if there is a CiliumEnvoyConfig that references this service.
-	cec, _, found := c.CECs.Get(txn, cecByServiceName(svc.Name))
+	cec, _, found := c.params.CECs.Get(txn, cecByServiceName(svc.Name))
 	if !found {
+		c.params.Log.Info("onServiceUpsert: CEC not found", "name", svc.Name)
 		return
 	}
 
@@ -299,7 +358,7 @@ func (c *cecController) onServiceUpsert(txn statedb.ReadTxn, svc *experimental.S
 	// service itself.
 
 	proxyPort := lookupProxyPort(cec, svcl)
-	c.Log.Debug("Setting proxy redirection (on service upsert)",
+	c.params.Log.Debug("Setting proxy redirection (on service upsert)",
 		"namespace", svcl.Namespace,
 		"name", svcl.Name,
 		"proxyPort", proxyPort,
