joamaki/cilium

commit 4219e5c3292612b7ecd221d39a683de1996ce71a

treewide: Relocate pkg/statedb to cilium/statedb
- Remove pkg/statedb and vendor cilium/statedb
- Rename imports of pkg/statedb to cilium/statedb
- Replace the StateDB Swagger API endpoints with the HTTP handler
  implemented in StateDB.
- Fix up the change from  DeleteTracker() to Changes() in StateDB
  API.
- Add metrics adapters for statedb and reconciler

Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/hive.go
@@ -12,6 +12,7 @@ import (
 	upstream "github.com/cilium/hive"
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/job"
+	"github.com/cilium/statedb"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/exp/slices"
 
@@ -20,8 +21,6 @@ import (
 	healthTypes "github.com/cilium/cilium/pkg/healthv2/types"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
-	"github.com/cilium/cilium/pkg/statedb"
-	"github.com/cilium/cilium/pkg/statedb/reconciler"
 )
 
 type (
@@ -44,9 +43,10 @@ func New(cells ...cell.Cell) *Hive {
 		healthv2.Cell,
 		job.Cell,
 		statedb.Cell,
-		reconciler.Cell,
 
 		cell.Provide(
+			NewStateDBMetrics,
+			NewStateDBReconcilerMetrics,
 			func() logrus.FieldLogger { return logging.DefaultLogger },
 			func(provider healthTypes.Provider) cell.Health {
 				return provider.ForModule(nil)
@@ -61,10 +61,16 @@ func New(cells ...cell.Cell) *Hive {
 			return hp.ForModule(fmid)
 		},
 	}
+	modulePrivateProviders := []cell.ModulePrivateProvider{
+		jobGroupProvider,
+		func(db *statedb.DB, mid cell.ModuleID) statedb.Handle {
+			return db.NewHandle(string(mid))
+		},
+	}
 	return upstream.NewWithOptions(
 		upstream.Options{
 			EnvPrefix:              "CILIUM_",
-			ModulePrivateProviders: []cell.ModulePrivateProvider{jobGroupProvider},
+			ModulePrivateProviders: modulePrivateProviders,
 			ModuleDecorators:       moduleDecorators,
 			DecodeHooks:            decodeHooks,
 			StartTimeout:           5 * time.Minute,

pkg/hive/reconciler_metrics.go
@@ -1,0 +1,160 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package hive
+
+import (
+	"time"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/statedb/reconciler"
+
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/metrics/metric"
+)
+
+type ReconcilerMetrics struct {
+	IncrementalReconciliationCount         metric.Vec[metric.Counter]
+	IncrementalReconciliationDuration      metric.Vec[metric.Observer]
+	IncrementalReconciliationTotalErrors   metric.Vec[metric.Counter]
+	IncrementalReconciliationCurrentErrors metric.Vec[metric.Gauge]
+
+	FullReconciliationCount          metric.Vec[metric.Counter]
+	FullReconciliationOutOfSyncCount metric.Vec[metric.Counter]
+	FullReconciliationTotalErrors    metric.Vec[metric.Counter]
+	FullReconciliationDuration       metric.Vec[metric.Observer]
+}
+
+const (
+	LabelModuleId  = "module_id"
+	LabelOperation = "op"
+)
+
+func NewStateDBReconcilerMetrics() (ReconcilerMetrics, reconciler.Metrics) {
+	m := ReconcilerMetrics{
+		IncrementalReconciliationCount: metric.NewCounterVec(metric.CounterOpts{
+			ConfigName: metrics.Namespace + "_reconciler_incremental_total",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "incremental_total",
+			Help:       "Number of incremental reconciliations performed",
+		}, []string{LabelModuleId}),
+
+		IncrementalReconciliationDuration: metric.NewHistogramVec(metric.HistogramOpts{
+			ConfigName: metrics.Namespace + "_reconciler_incremental_duration_seconds",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "incremental_duration_seconds",
+			Help:       "Histogram of per-operation duration during incremental reconciliation",
+		}, []string{LabelModuleId, LabelOperation}),
+
+		IncrementalReconciliationTotalErrors: metric.NewCounterVec(metric.CounterOpts{
+			ConfigName: metrics.Namespace + "_reconciler_incremental_errors_total",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "incremental_errors_total",
+			Help:       "Total number of errors encountered during incremental reconciliation",
+		}, []string{LabelModuleId}),
+
+		IncrementalReconciliationCurrentErrors: metric.NewGaugeVec(metric.GaugeOpts{
+			ConfigName: metrics.Namespace + "_reconciler_incremental_errors_current",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "incremental_errors_current",
+			Help:       "The number of objects currently failing to be reconciled",
+		}, []string{LabelModuleId}),
+
+		FullReconciliationCount: metric.NewCounterVec(metric.CounterOpts{
+			ConfigName: metrics.Namespace + "_reconciler_full_total",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "full_total",
+			Help:       "Number of full reconciliations performed",
+		}, []string{LabelModuleId}),
+
+		FullReconciliationOutOfSyncCount: metric.NewCounterVec(metric.CounterOpts{
+			ConfigName: metrics.Namespace + "_reconciler_full_out_of_sync_total",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "full_out_of_sync_total",
+			Help:       "Number of times full reconciliation found objects to reconcile",
+		}, []string{LabelModuleId}),
+
+		FullReconciliationTotalErrors: metric.NewCounterVec(metric.CounterOpts{
+			ConfigName: metrics.Namespace + "_reconciler_full_errors_total",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "full_errors_total",
+			Help:       "Total number of errors encountered during full reconciliation",
+		}, []string{LabelModuleId}),
+
+		FullReconciliationDuration: metric.NewHistogramVec(metric.HistogramOpts{
+			ConfigName: metrics.Namespace + "_reconciler_full_duration_seconds",
+			Disabled:   true,
+			Namespace:  metrics.Namespace,
+			Subsystem:  "reconciler",
+			Name:       "full_duration_seconds",
+			Help:       "Histogram of per-operation duration during full reconciliation",
+		}, []string{LabelModuleId, LabelOperation}),
+	}
+	return m, &reconcilerMetricsImpl{m}
+}
+
+type reconcilerMetricsImpl struct {
+	m ReconcilerMetrics
+}
+
+// FullReconciliationDuration implements reconciler.Metrics.
+func (m *reconcilerMetricsImpl) FullReconciliationDuration(moduleID cell.FullModuleID, operation string, duration time.Duration) {
+	if m.m.FullReconciliationDuration.IsEnabled() {
+		m.m.FullReconciliationDuration.WithLabelValues(LabelModuleId, moduleID.String(), LabelOperation, operation).
+			Observe(duration.Seconds())
+	}
+}
+
+// FullReconciliationErrors implements reconciler.Metrics.
+func (m *reconcilerMetricsImpl) FullReconciliationErrors(moduleID cell.FullModuleID, errs []error) {
+	if m.m.FullReconciliationCount.IsEnabled() {
+		m.m.FullReconciliationCount.WithLabelValues(LabelModuleId, moduleID.String())
+	}
+	if m.m.FullReconciliationTotalErrors.IsEnabled() {
+		m.m.FullReconciliationTotalErrors.WithLabelValues(moduleID.String()).Add(float64(len(errs)))
+	}
+}
+
+// FullReconciliationOutOfSync implements reconciler.Metrics.
+func (m *reconcilerMetricsImpl) FullReconciliationOutOfSync(moduleID cell.FullModuleID) {
+	if m.m.FullReconciliationOutOfSyncCount.IsEnabled() {
+		m.m.FullReconciliationOutOfSyncCount.WithLabelValues(LabelModuleId, moduleID.String()).Inc()
+	}
+}
+
+// IncrementalReconciliationDuration implements reconciler.Metrics.
+func (m *reconcilerMetricsImpl) IncrementalReconciliationDuration(moduleID cell.FullModuleID, operation string, duration time.Duration) {
+	if m.m.IncrementalReconciliationCount.IsEnabled() {
+		m.m.IncrementalReconciliationCount.WithLabelValues(LabelModuleId, moduleID.String()).Inc()
+	}
+	if m.m.IncrementalReconciliationDuration.IsEnabled() {
+		m.m.IncrementalReconciliationDuration.WithLabelValues(LabelModuleId, moduleID.String(), LabelOperation, operation).
+			Observe(duration.Seconds())
+	}
+}
+
+// IncrementalReconciliationErrors implements reconciler.Metrics.
+func (m *reconcilerMetricsImpl) IncrementalReconciliationErrors(moduleID cell.FullModuleID, errs []error) {
+	if m.m.IncrementalReconciliationCurrentErrors.IsEnabled() {
+		m.m.IncrementalReconciliationCurrentErrors.WithLabelValues(LabelModuleId, moduleID.String()).Set(float64(len(errs)))
+	}
+	if m.m.IncrementalReconciliationTotalErrors.IsEnabled() {
+		m.m.IncrementalReconciliationCurrentErrors.WithLabelValues(LabelModuleId, moduleID.String()).Add(float64(len(errs)))
+	}
+}
+
+var _ reconciler.Metrics = &reconcilerMetricsImpl{}

commit fb412727c3a2cbda6385585dd708fbcbdfa5f940

hive: Fix StateDB and reconciler metrics
Relocating StateDB to its own repo reworked how metrics were done.
Unfortunately the new metrics implementation was not properly tested
as it was disabled by default.

This fixes the multiple issues it had:
- metrics.Metric cell was never defined for the metrics, so they were not
  registered.
- ConfigName dropped as it is automatically computed if missing. There were
  few that had wrong ConfigName.
- Fix use of "full" instead of "prune" in the pruning metrics (leftover from
  when full reconciliation split into refresh and prune).
- WithLabelValues was called with the label names instead of just the values
- Some labels were wrong way around
- Prune count wasn't incremented

The implementation was tested locally with "kind" by enabling all the metrics
with "prometheus.metrics" helm option and then checking "cilium metrics list".

Fixes: 4219e5c32926 ("treewide: Relocate pkg/statedb to cilium/statedb")
Signed-off-by: Jussi Maki <jussi@isovalent.com>

pkg/hive/hive.go
@@ -21,6 +21,7 @@ import (
 	"github.com/cilium/cilium/pkg/hive/health/types"
 	"github.com/cilium/cilium/pkg/logging"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/metrics"
 )
 
 type (
@@ -40,18 +41,36 @@ func New(cells ...cell.Cell) *Hive {
 	cells = append(
 		slices.Clone(cells),
 
-		health.Cell,
 		job.Cell,
-		statedb.Cell,
 
+		// Module health
+		cell.Group(
+			health.Cell,
+			cell.Provide(
+				func(provider types.Provider) cell.Health {
+					return provider.ForModule(nil)
+				},
+			),
+		),
+
+		// StateDB and its metrics
+		cell.Group(
+			statedb.Cell,
+
+			metrics.Metric(NewStateDBMetrics),
+			metrics.Metric(NewStateDBReconcilerMetrics),
+			cell.Provide(
+				NewStateDBMetricsImpl,
+				NewStateDBReconcilerMetricsImpl,
+			),
+		),
+
+		// The root logrus FieldLogger.
 		cell.Provide(
-			NewStateDBMetrics,
-			NewStateDBReconcilerMetrics,
 			func() logrus.FieldLogger { return logging.DefaultLogger },
-			func(provider types.Provider) cell.Health {
-				return provider.ForModule(nil)
-			},
-		))
+		),
+	)
+
 	// Scope logging and health by module ID.
 	moduleDecorators := []cell.ModuleDecorator{
 		func(log logrus.FieldLogger, mid cell.ModuleID) logrus.FieldLogger {

pkg/hive/reconciler_metrics.go
@@ -25,76 +25,73 @@ type ReconcilerMetrics struct {
 }
 
 const (
-	LabelModuleId  = "module_id"
-	LabelOperation = "op"
+	labelModuleId  = "module_id"
+	labelOperation = "op"
 )
 
-func NewStateDBReconcilerMetrics() (ReconcilerMetrics, reconciler.Metrics) {
+func NewStateDBReconcilerMetrics() ReconcilerMetrics {
 	m := ReconcilerMetrics{
 		ReconciliationCount: metric.NewCounterVec(metric.CounterOpts{
-			ConfigName: metrics.Namespace + "_count",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "count",
-			Help:       "Number of reconciliation rounds performed",
-		}, []string{LabelModuleId}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "count",
+			Help:      "Number of reconciliation rounds performed",
+		}, []string{labelModuleId}),
 
 		ReconciliationDuration: metric.NewHistogramVec(metric.HistogramOpts{
-			ConfigName: metrics.Namespace + "_reconciler_duration_seconds",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "duration_seconds",
-			Help:       "Histogram of per-operation duration during reconciliation",
-		}, []string{LabelModuleId, LabelOperation}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "duration_seconds",
+			Help:      "Histogram of per-operation duration during reconciliation",
+		}, []string{labelModuleId, labelOperation}),
 
 		ReconciliationTotalErrors: metric.NewCounterVec(metric.CounterOpts{
-			ConfigName: metrics.Namespace + "_reconciler_errors_total",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "errors_total",
-			Help:       "Total number of errors encountered during reconciliation",
-		}, []string{LabelModuleId}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "errors_total",
+			Help:      "Total number of errors encountered during reconciliation",
+		}, []string{labelModuleId}),
 
 		ReconciliationCurrentErrors: metric.NewGaugeVec(metric.GaugeOpts{
-			ConfigName: metrics.Namespace + "_reconciler_errors_current",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "errors_current",
-			Help:       "The number of objects currently failing to be reconciled",
-		}, []string{LabelModuleId}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "errors_current",
+			Help:      "The number of objects currently failing to be reconciled",
+		}, []string{labelModuleId}),
 
 		PruneCount: metric.NewCounterVec(metric.CounterOpts{
-			ConfigName: metrics.Namespace + "_reconciler_prune_count",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "prune_count",
-			Help:       "Number of prunes performed",
-		}, []string{LabelModuleId}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "prune_count",
+			Help:      "Number of prunes performed",
+		}, []string{labelModuleId}),
 
 		PruneTotalErrors: metric.NewCounterVec(metric.CounterOpts{
-			ConfigName: metrics.Namespace + "_reconciler_prune_errors_total",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "full_errors_total",
-			Help:       "Total number of errors encountered during full reconciliation",
-		}, []string{LabelModuleId}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "prune_errors_total",
+			Help:      "Total number of errors encountered during pruning",
+		}, []string{labelModuleId}),
 
 		PruneDuration: metric.NewHistogramVec(metric.HistogramOpts{
-			ConfigName: metrics.Namespace + "_reconciler_prune_duration_seconds",
-			Disabled:   true,
-			Namespace:  metrics.Namespace,
-			Subsystem:  "reconciler",
-			Name:       "full_duration_seconds",
-			Help:       "Histogram of per-operation duration during full reconciliation",
-		}, []string{LabelModuleId, LabelOperation}),
+			Disabled:  true,
+			Namespace: metrics.Namespace,
+			Subsystem: "reconciler",
+			Name:      "prune_duration_seconds",
+			Help:      "Histogram of pruning duration",
+		}, []string{labelModuleId}),
 	}
-	return m, &reconcilerMetricsImpl{m}
+	return m
+}
+
+func NewStateDBReconcilerMetricsImpl(m ReconcilerMetrics) reconciler.Metrics {
+	return &reconcilerMetricsImpl{m}
 }
 
 type reconcilerMetricsImpl struct {
@@ -104,7 +101,7 @@ type reconcilerMetricsImpl struct {
 // PruneDuration implements reconciler.Metrics.
 func (m *reconcilerMetricsImpl) PruneDuration(moduleID cell.FullModuleID, duration time.Duration) {
 	if m.m.PruneDuration.IsEnabled() {
-		m.m.PruneDuration.WithLabelValues(LabelModuleId, moduleID.String()).
+		m.m.PruneDuration.WithLabelValues(moduleID.String()).
 			Observe(duration.Seconds())
 	}
 }
@@ -112,9 +109,9 @@ func (m *reconcilerMetricsImpl) PruneDuration(moduleID cell.FullModuleID, durati
 // FullReconciliationErrors implements reconciler.Metrics.
 func (m *reconcilerMetricsImpl) PruneError(moduleID cell.FullModuleID, err error) {
 	if m.m.PruneCount.IsEnabled() {
-		m.m.PruneCount.WithLabelValues(LabelModuleId, moduleID.String())
+		m.m.PruneCount.WithLabelValues(moduleID.String()).Inc()
 	}
-	if m.m.PruneTotalErrors.IsEnabled() {
+	if err != nil && m.m.PruneTotalErrors.IsEnabled() {
 		m.m.PruneTotalErrors.WithLabelValues(moduleID.String()).Add(1)
 	}
 }
@@ -122,10 +119,10 @@ func (m *reconcilerMetricsImpl) PruneError(moduleID cell.FullModuleID, err error
 // ReconciliationDuration implements reconciler.Metrics.
 func (m *reconcilerMetricsImpl) ReconciliationDuration(moduleID cell.FullModuleID, operation string, duration time.Duration) {
 	if m.m.ReconciliationCount.IsEnabled() {
-		m.m.ReconciliationCount.WithLabelValues(LabelModuleId, moduleID.String()).Inc()
+		m.m.ReconciliationCount.WithLabelValues(moduleID.String()).Inc()
 	}
 	if m.m.ReconciliationDuration.IsEnabled() {
-		m.m.ReconciliationDuration.WithLabelValues(LabelModuleId, moduleID.String(), LabelOperation, operation).
+		m.m.ReconciliationDuration.WithLabelValues(moduleID.String(), operation).
 			Observe(duration.Seconds())
 	}
 }
@@ -133,10 +130,10 @@ func (m *reconcilerMetricsImpl) ReconciliationDuration(moduleID cell.FullModuleI
 // ReconciliationErrors implements reconciler.Metrics.
 func (m *reconcilerMetricsImpl) ReconciliationErrors(moduleID cell.FullModuleID, new, current int) {
 	if m.m.ReconciliationCurrentErrors.IsEnabled() {
-		m.m.ReconciliationCurrentErrors.WithLabelValues(LabelModuleId, moduleID.String()).Set(float64(current))
+		m.m.ReconciliationCurrentErrors.WithLabelValues(moduleID.String()).Set(float64(current))
 	}
 	if m.m.ReconciliationTotalErrors.IsEnabled() {
-		m.m.ReconciliationCurrentErrors.WithLabelValues(LabelModuleId, moduleID.String()).Add(float64(new))
+		m.m.ReconciliationCurrentErrors.WithLabelValues(moduleID.String()).Add(float64(new))
 	}
 }
 

pkg/hive/statedb_metrics.go
@@ -34,6 +34,12 @@ type StateDBMetrics struct {
 	TableGraveyardCleaningDuration metric.Vec[metric.Observer]
 }
 
+const (
+	labelTable  = "table"
+	labelTables = "tables"
+	labelHandle = "handle"
+)
+
 type stateDBMetricsImpl struct {
 	m StateDBMetrics
 }
@@ -41,42 +47,42 @@ type stateDBMetricsImpl struct {
 // DeleteTrackerCount implements statedb.Metrics.
 func (i stateDBMetricsImpl) DeleteTrackerCount(tableName string, numTrackers int) {
 	if i.m.TableDeleteTrackerCount.IsEnabled() {
-		i.m.TableDeleteTrackerCount.WithLabelValues("table", tableName).Set(float64(numTrackers))
+		i.m.TableDeleteTrackerCount.WithLabelValues(tableName).Set(float64(numTrackers))
 	}
 }
 
 // GraveyardCleaningDuration implements statedb.Metrics.
 func (i stateDBMetricsImpl) GraveyardCleaningDuration(tableName string, duration time.Duration) {
 	if i.m.TableGraveyardCleaningDuration.IsEnabled() {
-		i.m.TableGraveyardCleaningDuration.WithLabelValues("table", tableName).Observe(float64(duration.Seconds()))
+		i.m.TableGraveyardCleaningDuration.WithLabelValues(tableName).Observe(float64(duration.Seconds()))
 	}
 }
 
 // GraveyardLowWatermark implements statedb.Metrics.
 func (i stateDBMetricsImpl) GraveyardLowWatermark(tableName string, lowWatermark uint64) {
 	if i.m.TableGraveyardLowWatermark.IsEnabled() {
-		i.m.TableGraveyardLowWatermark.WithLabelValues("table", tableName).Set(float64(lowWatermark))
+		i.m.TableGraveyardLowWatermark.WithLabelValues(tableName).Set(float64(lowWatermark))
 	}
 }
 
 // GraveyardObjectCount implements statedb.Metrics.
 func (i stateDBMetricsImpl) GraveyardObjectCount(tableName string, numDeletedObjects int) {
 	if i.m.TableGraveyardObjectCount.IsEnabled() {
-		i.m.TableGraveyardObjectCount.WithLabelValues("table", tableName).Set(float64(numDeletedObjects))
+		i.m.TableGraveyardObjectCount.WithLabelValues(tableName).Set(float64(numDeletedObjects))
 	}
 }
 
 // ObjectCount implements statedb.Metrics.
 func (i stateDBMetricsImpl) ObjectCount(tableName string, numObjects int) {
 	if i.m.TableObjectCount.IsEnabled() {
-		i.m.TableObjectCount.WithLabelValues("table", tableName).Set(float64(numObjects))
+		i.m.TableObjectCount.WithLabelValues(tableName).Set(float64(numObjects))
 	}
 }
 
 // Revision implements statedb.Metrics.
 func (i stateDBMetricsImpl) Revision(tableName string, revision uint64) {
 	if i.m.TableRevision.IsEnabled() {
-		i.m.TableRevision.WithLabelValues("table", tableName).Set(float64(revision))
+		i.m.TableRevision.WithLabelValues(tableName).Set(float64(revision))
 	}
 }
 
@@ -84,8 +90,7 @@ func (i stateDBMetricsImpl) Revision(tableName string, revision uint64) {
 func (i stateDBMetricsImpl) WriteTxnDuration(handle string, tables []string, acquire time.Duration) {
 	if i.m.WriteTxnDuration.IsEnabled() {
 		i.m.WriteTxnDuration.WithLabelValues(
-			"handle", handle,
-			"tables", strings.Join(tables, ","),
+			handle, strings.Join(tables, ","),
 		).Observe(acquire.Seconds())
 	}
 }
@@ -93,10 +98,7 @@ func (i stateDBMetricsImpl) WriteTxnDuration(handle string, tables []string, acq
 // WriteTxnTableAcquisition implements statedb.Metrics.
 func (i stateDBMetricsImpl) WriteTxnTableAcquisition(handle string, tableName string, acquire time.Duration) {
 	if i.m.TableContention.IsEnabled() {
-		i.m.TableContention.WithLabelValues(
-			"handle", handle,
-			"table", tableName,
-		)
+		i.m.TableContention.WithLabelValues(handle, tableName)
 	}
 }
 
@@ -104,79 +106,82 @@ func (i stateDBMetricsImpl) WriteTxnTableAcquisition(handle string, tableName st
 func (i stateDBMetricsImpl) WriteTxnTotalAcquisition(handle string, tables []string, acquire time.Duration) {
 	if i.m.WriteTxnAcquisition.IsEnabled() {
 		i.m.WriteTxnAcquisition.WithLabelValues(
-			"handle", handle,
-			"tables", strings.Join(tables, ","),
+			handle, strings.Join(tables, ","),
 		)
 	}
 }
 
 var _ statedb.Metrics = stateDBMetricsImpl{}
 
-func NewStateDBMetrics() (StateDBMetrics, statedb.Metrics) {
+func NewStateDBMetrics() StateDBMetrics {
 	m := StateDBMetrics{
 		WriteTxnDuration: metric.NewHistogramVec(metric.HistogramOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "write_txn_duration_seconds",
 			Help:      "How long a write transaction was held.",
 			Disabled:  true,
-		}, []string{"tables", "handle"}),
+		}, []string{labelHandle, labelTables}),
 		WriteTxnAcquisition: metric.NewHistogramVec(metric.HistogramOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "write_txn_acquisition_seconds",
 			Help:      "How long it took to acquire a write transaction for all tables.",
 			Disabled:  true,
-		}, []string{"tables", "handle"}),
+		}, []string{labelHandle, labelTables}),
 		TableContention: metric.NewGaugeVec(metric.GaugeOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_contention_seconds",
 			Help:      "How long writers were blocked while waiting to acquire a write transaction for a specific table.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelHandle, labelTable}),
 		TableObjectCount: metric.NewGaugeVec(metric.GaugeOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_objects",
 			Help:      "The amount of objects in a given table.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelTable}),
 		TableRevision: metric.NewGaugeVec(metric.GaugeOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_revision",
 			Help:      "The current revision of a given table.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelTable}),
 		TableDeleteTrackerCount: metric.NewGaugeVec(metric.GaugeOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_delete_trackers",
 			Help:      "The amount of delete trackers for a given table.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelTable}),
 		TableGraveyardObjectCount: metric.NewGaugeVec(metric.GaugeOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_graveyard_objects",
 			Help:      "The amount of objects in the graveyard for a given table.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelTable}),
 		TableGraveyardLowWatermark: metric.NewGaugeVec(metric.GaugeOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_graveyard_low_watermark",
 			Help:      "The lowest revision of a given table that has been processed by the graveyard garbage collector.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelTable}),
 		TableGraveyardCleaningDuration: metric.NewHistogramVec(metric.HistogramOpts{
-			Namespace: metrics.CiliumAgentNamespace,
+			Namespace: metrics.Namespace,
 			Subsystem: "statedb",
 			Name:      "table_graveyard_cleaning_duration_seconds",
 			Help:      "The time it took to clean the graveyard for a given table.",
 			Disabled:  true,
-		}, []string{"table"}),
+		}, []string{labelTable}),
 	}
-	return m, stateDBMetricsImpl{m}
+	return m
+}
+
+func NewStateDBMetricsImpl(m StateDBMetrics) statedb.Metrics {
+	return stateDBMetricsImpl{m}
 }